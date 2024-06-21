7b1ca49c131343498300f15d571d480bededb316
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:38:22 +0700

    update and commit

--- problem2/main.py
+++ problem2/main.py
@@ -1,5 +1,14 @@
 def draw_xyz(N):
     pattern = ""
+    for i in range(1, N*N + 1):
+        if i % 3 == 0:
+            pattern += "X "
+        elif i % 2 == 1:
+            pattern += "Y "
+        else:
+            pattern += "Z "
+        if i % N == 0:
+            pattern += "\n"
     return pattern
 
 if __name__ == '__main__':
