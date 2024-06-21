e20d8533071c675e88e292c34af288a22163cdff
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:37:54 +0700

    update and commit

--- problem1/main.py
+++ problem1/main.py
@@ -1,6 +1,11 @@
 def play_with_asterisk(n):
     pattern = ""
+    for i in range(1,n+1):
+        pattern += " " * (n - i)
+        pattern += "* " * i
+        pattern += "\n"
     return pattern
+
 
 if __name__ == '__main__':
     print(play_with_asterisk(5))
