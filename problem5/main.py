aa069f47e8cdb772cb0f23ab565c5e369ed6e48c
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:39:26 +0700

    update and commit

--- problem5/main.py
+++ problem5/main.py
@@ -1,7 +1,15 @@
 def mean_median(array_input):
     mean = 1.0
     median = 1
-    return (mean, median)
+    x = len(array_input)
+    if x == 0:
+        return None
+    elif x % 2 == 0:
+        median = (array_input[x//2]+array_input[x//2-1])/2
+    else :
+        median = array_input[x//2]
+    mean = sum(array_input)/x
+    return round(mean, 1), round(median, 1)
 
 if __name__ == '__main__':
     print(mean_median([1, 2, 3, 4])) # (2.5, 2.5)
