e107425613129799bbf726bdd511417fef9cc365
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:41:14 +0700

    Add files via upload

---
+++ problem2/problem2.py
@@ -0,0 +1,30 @@
+def draw_xyz(N):
+    pattern = ""
+    for i in range(1, N*N + 1):
+        if i % 3 == 0:
+            pattern += "X "
+        elif i % 2 == 1:
+            pattern += "Y "
+        else:
+            pattern += "Z "
+        if i % N == 0:
+            pattern += "\n"
+    return pattern
+
+if __name__ == '__main__':
+    print(draw_xyz(3))
+    """
+    Y Z X
+    Z Y X
+    Y Z X
+    """
+
+
+    print(draw_xyz(5))
+    """
+    Y Z X Z Y
+    X Y Z X Z
+    Y X Y Z X
+    Z Y X Y Z
+    X Z Y X Y
+    """
